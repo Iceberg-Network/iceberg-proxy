e2db54a9576a2b78200def679758012153956c38
Preston <PrestonT500WasTaken@proton.me>
2024-05-17 11:05:56 -0700

    Making the copy and paste easier didnt work so uncommit

--- js/navbarloading.js
+++ js/navbarloading.js
@@ -1,13 +1,3 @@
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "https://code.jquery.com/jquery-1.10.2.js";
-
-script.onreadystatechange = handler;
-script.onload = handler;
-
-head.appendChild(script);
-
 $(function(){
     $("#navbar-placeholder").load("../navbar.html");
 });
