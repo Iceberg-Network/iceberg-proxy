9a11384e5edd348ad75a7239fd8bffc05540c9a5
accessgames <83598795+accessgames@users.noreply.github.com>
2024-05-24 08:54:33 -0700

    Update betatestalert.js

--- js/betatestalert.js
+++ js/betatestalert.js
@@ -1,8 +1,8 @@
-
 var result = confirm("Commander v2 would like users to work with us to help improve the website by giving us feedback! Would you like to work with us and help Commander? ");
 
 if (result) {
-  // User clicked "OK"
-  window.location.href = 'https://forms.google.com';
+  
+  window.open('https://forms.google.com', '_blank');
 } else {
+  
 }
