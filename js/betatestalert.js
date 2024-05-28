001263d654739f928f3923ae5577d7d6dc586967
accessgames <83598795+accessgames@users.noreply.github.com>
2024-05-28 11:39:21 -0700

    Delete js/betatestalert.js

--- js/betatestalert.js
+++
@@ -1,8 +0,0 @@
-var result = confirm("Commander v2 would like users to work with us to help improve the website by giving us feedback! Would you like to work with us and help Commander? ");
-
-if (result) {
-  
-  window.open('https://forms.google.com', '_blank');
-} else {
-  
-}
